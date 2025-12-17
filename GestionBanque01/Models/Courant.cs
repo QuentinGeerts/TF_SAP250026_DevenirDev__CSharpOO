17fe8b825b867624007d8ca800bdfdce6a7af453
Quentin Geerts <geerts.quentin@gmail.com>
2025-12-17 17:04:21 +0100

    Correction de l'exercice gestion banque

---
+++ GestionBanque01/Models/Courant.cs
@@ -0,0 +1,35 @@
+﻿namespace GestionBanque.Models;
+
+public class Courant
+{
+    // Champs
+    private double _ligneDeCredit;
+
+
+    // Propriétés
+    public string Numero { get; set; }
+    public double Solde { get; private set; } // Lecture seule
+    public double LigneDeCredit
+    {
+        get { return _ligneDeCredit; }
+        set {
+            if (value < 0) return; // À remplacer plus tard par une exception
+            _ligneDeCredit = value;
+        }
+    }
+    public Personne Titulaire { get; set; }
+
+
+    // Méthodes
+    public void Depot(double montant)
+    {
+        if (montant <= 0) return; // À remplacer plus tard par une exception
+        Solde = Solde + montant;
+    }
+    public void Retrait(double montant)
+    {
+        if (montant <= 0) return; // À remplacer plus tard par une exception
+        if (Solde - montant < -LigneDeCredit) return; // À remplacer plus tard par une exception
+        Solde -= montant;
+    }
+}
---
+++ GestionBanque01/Program.cs
@@ -0,0 +1,43 @@
+﻿using GestionBanque.Models;
+
+
+Personne p1 = new Personne
+{
+    Nom = "Doe",
+    Prenom = "John",
+    DateNaiss = DateTime.Now
+};
+
+Personne p2 = new Personne
+{
+    Nom = "Geerts",
+    Prenom = "Quentin",
+    DateNaiss = new DateTime(1996, 4, 3)
+};
+
+Courant c1 = new Courant
+{
+    Numero = "00001",
+    Titulaire = p1,
+    LigneDeCredit = 0
+};
+
+
+Courant c2 = new Courant
+{
+    Numero = "00002",
+    Titulaire = p2,
+    LigneDeCredit = 200
+};
+
+c1.Depot(500);
+c2.Depot(-500);
+
+Console.WriteLine($"c1: {c1.Solde}");
+Console.WriteLine($"c2: {c2.Solde}");
+
+c1.Retrait(500);
+c2.Retrait(200);
+
+Console.WriteLine($"c1: {c1.Solde}");
+Console.WriteLine($"c2: {c2.Solde}");
