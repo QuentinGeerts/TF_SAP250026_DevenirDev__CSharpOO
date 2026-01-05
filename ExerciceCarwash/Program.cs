23691e8e7cdaab8f1797c90dbdfe8eb8c5f63a2d
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-05 17:01:12 +0100

    Correction 1 et 2 sur le Carwash (Délégués)

---
+++ ExerciceCarwash/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExerciceCarwash.Models;
+
+Carwash carwash = new Carwash();
+
+
+Voiture v1 = new("2-EWB-988");
+Voiture v2 = new("2-EWB-989");
+Voiture v3 = new("2-EWB-990");
+
+carwash.Traiter(v1);
+carwash.Traiter(v2);
+carwash.Traiter(v3);
---
+++ ExerciceCarwashV2/Models/Carwash.cs
@@ -0,0 +1,22 @@
+﻿namespace ExerciceCarwash.Models;
+
+public delegate void CarwashDelegate(Voiture v);
+
+internal class Carwash
+{
+    private CarwashDelegate traitements = null;
+
+    public Carwash()
+    {
+        traitements += delegate (Voiture v) { Console.WriteLine($"je prépare la voiture : {v.Plaque}"); };
+        traitements += (Voiture v) => { Console.WriteLine($"je lave la voiture : {v.Plaque}"); };
+        traitements += (Voiture v) => Console.WriteLine($"je sèche la voiture : {v.Plaque}");
+        traitements += v => Console.WriteLine($"je finalise la voiture : {v.Plaque}");
+    }
+
+    public void Traiter(Voiture v)
+    {
+        Console.WriteLine($"Traitement de la voiture {v.Plaque}:");
+        traitements?.Invoke(v);
+    }
+}
